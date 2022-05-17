dc43c9a83b3e6dc65b8d227f995e9151cf5e345d
Ajay Kumar MK <ajaykumarmkaju@gmail.com>
2022-05-17 23:41:44 +0530

    Added first question

---
+++ codewars/js/number-to-reversedArray.js
@@ -0,0 +1,7 @@
+// Convert number to reversed array of digits
+
+// Instructions
+
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+
