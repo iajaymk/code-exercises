961074e06544465c5400619863bd05e8d8080ccf
Ajay Kumar MK <ajaykumarmkaju@gmail.com>
2022-05-17 23:53:27 +0530

    Added number to reverse array of digits question

--- codewars/js/number-to-reversedArray.js
+++ codewars/js/number-to-reversedArray.js
@@ -5,3 +5,11 @@
 // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 
 
+function digitize(n) {
+    const arrOfStr = n.toString().split('').reverse()
+    const arrNum = []
+    arrOfStr.forEach(str => {
+    arrNum.push(Number(str));
+  });
+    return arrNum
+  }
