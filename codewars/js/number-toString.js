a05bd219c61f2842b4be24bf297c5169bfb720e3
Ajay Kumar MK <ajaykumarmkaju@gmail.com>
2022-05-18 00:57:33 +0530

    Added number to String

---
+++ codewars/js/number-toString.js
@@ -0,0 +1,13 @@
+// Convert a Number to a String!
+
+
+// Instructions
+
+// We need a function that can transform a number into a string.
+
+
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString()
+  }
