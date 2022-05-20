545eab2021b1809d8c913b50be7348624a86226c
Ajay Kumar MK <ajaykumarmkaju@gmail.com>
2022-05-20 20:18:03 +0530

    Added string endswith

---
+++ js/20-05-2022/stringendswith.js
@@ -0,0 +1,25 @@
+// String Ends with
+
+// Instructions 
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+
+function solution(str, ending){
+    // TODO: complete
+    let last=""
+    for(let i =str.length-ending.length;i<str.length;i++){
+      last += str[i] 
+    }
+    if(last==ending){
+      return true
+    }
+    else{
+      return false
+    }
+  }
