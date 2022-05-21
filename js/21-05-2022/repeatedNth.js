4280c74703716e45cebf065e954abdf150c88655
Ajay Kumar MK <ajaykumarmkaju@gmail.com>
2022-05-21 23:30:11 +0530

    Added another one

---
+++ js/21-05-2022/repeatedNth.js
@@ -0,0 +1,12 @@
+function deleteNth(arr,n){
+    // ...
+    let indices=[], count=0
+    for(let i=0;i<arr.length;i++){
+      if(arr.includes(a[i],i+1)){
+        count += 1
+        if(!indices.includes(i)){
+          indices.push(i)
+        }
+      }
+    }
+  }
