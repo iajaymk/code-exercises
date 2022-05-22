f27a13aba95e6dfeb4e4516d186d9ff0cd996ad0
Ajay Kumar MK <ajaykumarmkaju@gmail.com>
2022-05-22 19:54:16 +0530

    Added one more

--- js/21-05-2022/repeatedNth.js
+++ js/21-05-2022/repeatedNth.js
@@ -1,12 +1,13 @@
-function deleteNth(arr,n){
-    // ...
-    let indices=[], count=0
-    for(let i=0;i<arr.length;i++){
-      if(arr.includes(a[i],i+1)){
-        count += 1
-        if(!indices.includes(i)){
-          indices.push(i)
-        }
-      }
-    }
-  }
+// function deleteNth(arr,n){
+//     // ...
+//     let indices=[], count=0
+//     for(let i=0;i<arr.length;i++){
+//       if(arr.includes(a[i],i+1)){
+//         count += 1
+//         if(!indices.includes(i)){
+//           indices.push(i)
+//         }
+//       }
+//     }
+//   }
+// Errored ..Incorrect soln 
